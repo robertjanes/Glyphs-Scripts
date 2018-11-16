68d117984de5c6452f937126a180864877ba01d3
robertjanes <robertjanes2@gmail.com>
2018-11-16 20:29:19 +0100

    Added save location dialog to CreateInDesignTaggedText

--- CreateInDesignTaggedText.py
+++ CreateInDesignTaggedText.py
@@ -6,12 +6,19 @@ Create inDesign Tagged Text
 # Author: Robert Janes
 # robertjanes.com.au
 
-textString = "<ASCII-MAC>\n"
-textString += "<Version:5><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0,0,0,1>>"
-textString += "<DefineParaStyle:FontTableStyle=<Nextstyle:FontTableStyle><cLigatures:0><cFont:" + Font.familyName + "><cOTFContAlt:0>>"
-textString += "<ParaStyle:FontTableStyle>"
+import GlyphsApp
+import os
+saveLocation = GlyphsApp.GetFolder()
 
-for i, glyph in enumerate(Font.glyphs):
-	textString += "<cSpecialGlyph:" + str(i) + "><0xFFFD> <cSpecialGlyph:>"
+if saveLocation != None:
+	textString = "<ASCII-MAC>\n"
+	textString += "<Version:5><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0,0,0,1>>"
+	textString += "<DefineParaStyle:FontTableStyle=<Nextstyle:FontTableStyle><cLigatures:0><cFont:" + Font.familyName + "><cOTFContAlt:0>>"
+	textString += "<ParaStyle:FontTableStyle>"
 
-print textString
+	for i, glyph in enumerate(Font.glyphs):
+		textString += "<cSpecialGlyph:" + str(i) + "><0xFFFD> <cSpecialGlyph:>"
+
+	f = open(os.path.join(saveLocation, Font.familyName.replace(' ', '') + '-inDesignTaggedText.txt'), "w")
+	f.write(textString)
+	f.close()
