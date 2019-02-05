217f2babfdc53c49433ee4553ba438d233862203
Robert Janes <robertjanes2@gmail.com>
2019-02-05 16:22:39 +0100

    Added InstanceInterpolationsReport

---
+++ InstanceInterpolationsReport.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#MenuTitle: Instance Interpolations Report
+__doc__="""
+Instance Interpolations Report
+"""
+import vanilla
+
+text = ""
+for instance in Font.instances:
+	if instance.active is False: continue
+	text += "%s Instance is interpolated with:\n" % (instance.name)
+	for instanceInterpolation in instance.instanceInterpolations:
+		text += u"• %s: %s Master\n" % ("{0:.2f}".format(instance.instanceInterpolations[instanceInterpolation]), Font.masters[instanceInterpolation].name)
+	text += "\n"
+
+class InstanceInterpolationsReport(object):
+
+    def __init__(self):
+        self.w = Window((350, 600),
+        			    "Instance Interpolations Report",
+        			    minSize=(300, 450))
+        self.w.textEditor = TextEditor((10, 10, -10, -10),
+                            text=text,
+                            readOnly=True)
+        self.w.open()
+
+InstanceInterpolationsReport()
--- README.md
+++ README.md
@@ -12,11 +12,14 @@ Creates an inDesign Tagged Text document containing all glyphs in the font.
 #### Delete Background Layers
 Clears the background layers of the selected layers.
 
-### Find Bracket and Brace Layers
+#### Find Bracket and Brace Layers
 Logs the names of all bracket and brace layers to the console.
 
 #### HalfAllKerningValues
 Halves the values of all kerning pairs in all masters.
+
+#### InstanceInterpolationsReport
+View a report displaying each instances interpolation coefficients.
 
 #### SwapGlyphNames
 Swaps the names of 2 glyphs. Useful for switching alternative, ssXX glyphs.
