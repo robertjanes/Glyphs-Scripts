a44097551ef8a95461141582cd6fc2808c3e397a
Robert Janes <robertjanes2@gmail.com>
2025-05-26 13:46:43 +0200

    Updates

--- CloseOpenCorners.py
+++ CloseOpenCorners.py
@@ -1,27 +1,22 @@
 # MenuTitle: Close Open Corners
 # -*- coding: utf-8 -*-
 __doc__ = """
-Close Open Corners
+Close all open corners in the current font.
 """
-import GlyphsApp
-
-Font = GlyphsApp.currentFont()
-
 Glyphs.clearLog()
-from GlyphsApp import GSPathPen
+from GlyphsApp import GSPathPen, GSPath
+import copy
 
 for glyph in Font.glyphs:
     for layer in glyph.layers:
-        thisLayer = layer
-
-        layerPaths = [l for l in thisLayer.paths]
-
-        thisLayer.paths = []
-
-        for eachPath in layerPaths:
-            pen = GSPathPen.alloc().init()
-            eachPath.drawInPen_(pen)
-            eachPath = pen.layer().paths
-            eachPath = eachPath[0]
-            thisLayer.addPath_(eachPath)
-            # thisLayer.cleanUpPaths()
+        newShapes = []
+        for s, shape in enumerate(layer.shapes):
+            if isinstance(shape, GSPath) is False:
+                newShapes.append(shape)
+                continue
+            pen = GSPathPen.new()
+            path = copy.copy(shape)
+            path.drawInPen_(pen)
+            path = pen.layer().paths[0]
+            newShapes.append(path)
+        layer.shapes = newShapes
